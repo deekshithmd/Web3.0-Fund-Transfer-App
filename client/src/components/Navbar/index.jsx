5f496335e2413f90f6dff10e693fb56356bd1afa
deekshithmd <deekshithmogra@gmail.com>
2023-01-12 10:31:50 +0530

    added navbar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,1 +1,58 @@
-export const Navbar = () => <h1>Navbar</h1>;
+import React from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import logo from "../../assets/krypt.png";
+
+const NavItem = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
+
+export const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+          {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+            <NavItem key={index + item} title={item} />
+          ))}
+          <li className="bg-[#2952e3] py-2 px-4 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+            Login
+          </li>
+        </ul>
+        <div className="flex relative">
+          {toggleMenu ? (
+            <AiOutlineClose
+              fontSize={28}
+              className="text-white md:hidden cursor-pointer"
+              onClick={() => setToggleMenu(!toggleMenu)}
+            />
+          ) : (
+            <HiMenuAlt4
+              fontSize={28}
+              className="text-white md:hidden cursor-pointer"
+              onClick={() => setToggleMenu(!toggleMenu)}
+            />
+          )}
+          {toggleMenu && (
+            <ul className="z-10 fixed top-0 right-2 p-3">
+              <li className="text-xl w-full my-2">
+                <AiOutlineClose onClick={() => setToggleMenu(false)} />
+              </li>
+              {["Market", "Exchange", "Tutorials", "Wallets"].map(
+                (item, index) => (
+                  <NavItem
+                    key={index + item}
+                    title={item}
+                    classProps="my-2 text-lg"
+                  />
+                )
+              )}
+            </ul>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
