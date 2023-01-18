0289211f12f484f70cf4ea4ebb23c66744b07306
deekshithmd <deekshithmogra@gmail.com>
2023-01-18 10:30:54 +0530

    added transactions

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,1 +1,13 @@
-export const Footer = () => <h1>Footer</h1>;
+import logo from "../../assets/krypt.png";
+
+export const Footer = () => {
+  return (
+    <div className="w-full flex md:justify-center justify-between items-center p-4 gradient-bg-footer">
+      <div className="flex w-full sm:flex-row flex-col justify-between items-center my-4">
+        <div className="flex flex-[0.5] justify-center items-center">
+          <img src={logo} alt="logo" className="w-32" />
+        </div>
+      </div>
+    </div>
+  );
+};
